b3f4c53f80e4264f2a97eafa41e1472420a3c6f6
MUSTAFA <MUSTAFA@DESKTOP-TIU0DO2>
2023-06-10 01:31:27 +0300

    kural değiştirildi

--- src/DhontSystem.java
+++ src/DhontSystem.java
@@ -1,40 +1,40 @@
 public class DhontSystem {
     public static void main(String[] args) {
         // Party votes and total number of seats
-        int[] partyVotes = {10000, 5000, 3000, 2000};
-        int totalSeats = 10;
-        String[] parties = {"A partisi", "B partisi", "C partisi", "D partisi"};
+        int[] partioylari = {10000, 5000, 3000, 2000};
+        int toplamsandalyesayisi = 10;
+        String[] partiler = {"A partisi", "B partisi", "C partisi", "D partisi"};
 
         // Allocate seats using the D'Hondt method
-        int[] partySeats = allocateSeats(partyVotes, totalSeats);
+        int[] partisandalyaleri = atama(partioylari, toplamsandalyesayisi);
 
         // Output the results
-        for (int i = 0; i < partySeats.length; i++) {
-            System.out.println(parties[i] + " - Seats: " + partySeats[i]);
+        for (int i = 0; i < partisandalyaleri.length; i++) {
+            System.out.println(partiler[i] + " - sandalye: " + partisandalyaleri[i]);
         }
     }
 
-    public static int[] allocateSeats(int[] partyVotes, int totalSeats) {
+    public static int[] atama(int[] partioyları, int toplamsandalye) {
         // Array to store the number of seats for each party
-        int[] partySeats = new int[partyVotes.length];
+        int[] sandalyaler = new int[partioyları.length];
 
-        for (int i = 0; i < totalSeats; i++) {
+        for (int i = 0; i < toplamsandalye; i++) {
             // Find the party with the highest votes per seat ratio
-            int maxIndex = 0;
-            int maxVotes = partyVotes[0] / (partySeats[0] + 1);
+            int dizi = 0;
+            int encokoyalan = partioyları[0] / (sandalyaler[0] + 1);
 
-            for (int j = 1; j < partyVotes.length; j++) {
-                int votesPerSeat = partyVotes[j] / (partySeats[j] + 1);
-                if (votesPerSeat > maxVotes) {
-                    maxVotes = votesPerSeat;
-                    maxIndex = j;
+            for (int j = 1; j < partioyları.length; j++) {
+                int oylar = partioyları[j] / (sandalyaler[j] + 1);
+                if (oylar > encokoyalan) {
+                    encokoyalan = oylar;
+                    dizi = j;
                 }
             }
 
             // Allocate a seat to the party with the highest ratio
-            partySeats[maxIndex]++;
+            sandalyaler[dizi]++;
         }
 
-        return partySeats;
+        return sandalyaler;
     }
 }
